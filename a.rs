58f54ed5daf4ebadc64a0bc68d24cbe9abe30a0b
DarumaDocker.dev <shishuo.wang@gmail.com>
2023-04-17 14:50:05 +0800

    Update a.rs

--- a.rs
+++ a.rs
@@ -27,3 +27,11 @@ fn fizzbuzz(n: u32) -> () {
         println!("{}", n);
     }
 }
+
+// When a function returns `()`, the return type can be omitted from the
+// signature
+fn fizzbuzz_to(n: u32) {
+    for n in 1..=n {
+        fizzbuzz(n);
+    }
+}
